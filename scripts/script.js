7817ed5dd07b9a6936450f6219c65a539b7cac36
nazir <you@example.com>
2026-03-09 20:47:25 +0600

    issue card design

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const mainContainer = document.getElementById("main-container");
 
-function login(){
-if(document.getElementById("usr").value === "admin" && document.getElementById("pwd").value === "admin123"){
+function login() {
+    if (document.getElementById("usr").value === "admin" && document.getElementById("pwd").value === "admin123") {
         //mainContainer.innerHTML = "";
         window.location.href = "issues.html";
     }
